6b3415cff7712e98bd0e164e756c6234daaf4aac
Samuel Cornet <samuel@noukod.com>
2019-01-09 14:55:53 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,26 @@
 require_relative 'config/environment'
 
+# configure do
+#   enable :sessions
+#   set :session_secret, "secret"
+# end
+
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, ENV.fetch('SESSION_SECRET') {SecureRandom.hex(64)}
+  end
+
+  get '/' do
+    @session = session
+    erb :index
+  end
+
+  post '/checkout' do
+    session[:item] = params[:item]
+    @session = session
+    "#{@session[:item]}"
+    # erb :checkout
+  end
 end
