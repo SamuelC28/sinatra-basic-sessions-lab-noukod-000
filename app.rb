7e94a1f18194d4e3b606b4f6285ba6d066f6e97d
Samuel Cornet <samuel@noukod.com>
2019-01-09 14:36:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,6 +7,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+  
   get '/' do
     @session = session
     erb :index
